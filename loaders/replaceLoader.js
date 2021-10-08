16e0327ee3f0eea9189a6a41039c9354b44651e7
cjshinningg <784319417@qq.com>
2021-10-08 22:18:37 +0800

    6-2 如何编写一个Loader（2）

--- loaders/replaceLoader.js
+++ loaders/replaceLoader.js
@@ -1,8 +1,5 @@
 const loaderUtils = require('loader-utils');
 
 module.exports = function (source) {
-  const options = loaderUtils.getOptions(this);
-  // return source.replace('jenny', options.name);
-  const result = source.replace('jenny', options.name);
-  this.callback(null, result);
+  return source.replace('chan', 'world');
 }
